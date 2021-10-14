b87cf05d9ff8d154eb31624b156e30334a10a7bd
Brian Mikinski <bmikinski@basisplanning.com>
2021-10-14 09:02:51 -0500

    Update for build

--- Program.cs
+++ Program.cs
@@ -1,86 +1,138 @@
-﻿
+﻿Console.WriteLine("Hello, World to Covariant Property Return Types!");
 
+var hondaCrv = new Suv(new FourSpeed());
+hondaCrv.GoForward();
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+public class Vehicle<T> where T : GearBox
+{
 
+    /// <summary>
+    /// This is necessary because C# 10 was not able to include "required" parameters.
+    /// Required properites were unfortunately not added to C# 10. See https://github.com/dotnet/csharplang/issues/3630
+    /// </summary>
+    /// <param name="transmission"></param>
+    public Vehicle(T transmission)
+    {
+        Transmission = transmission;
+    }
 
-public class Vehicle<T> where T : GearBox {
-    public T Transmission {get; init;}
+    protected T Transmission { get; init; }
 
-    public virtual GearBox GetTransmission() {
+    /// <summary>
+    /// Engage the
+    /// </summary>
+    public virtual T GoForward()
+    {
+        Transmission.Engage();
+
         return Transmission;
     }
 }
 
-public class Suv : Vehicle<FourSpeed> {
-
-    public override FourSpeed GetTransmission()
-    {
-        return base.Transmission;
-    }
-
-}
-
-public class Truck : Suv {
-    public override FourSpeed GetTransmission()
-    {
-        return base.Transmission;
-    }
-}
-
-public abstract class GearBox {
-
-    public abstract void ShiftGear();
-}
-
-public class FourSpeed : GearBox {
-
-    public override void ShiftGear() {
-        Console.WriteLine("4 Speed Shifting");
-    }
-}
-
-public class SixSpeed : GearBox
+public class Suv : Vehicle<FourSpeed>
 {
-    public override void ShiftGear()
+    public Suv(FourSpeed transmission) : base(transmission)
     {
-         Console.WriteLine("4 Speed Shifting");
+
+    }
+
+    public override FourSpeed GoForward()
+    {
+        var transmission = base.GoForward();
+        transmission.DisengageClutch();
+        return transmission;
+    }
+}
+
+public class Truck : Vehicle<SixSpeed>
+{
+    public Truck(SixSpeed transmission) : base(transmission)
+    {
+
+    }
+
+    public override SixSpeed GoForward()
+    {
+        var transmission = base.GoForward();
+        transmission.Enable4WheelDrive();
+        return transmission;
+    }
+}
+
+public abstract class GearBox
+{
+
+    public abstract void Engage();
+}
+
+public class FourSpeed : GearBox
+{
+
+    public override void Engage()
+    {
+        Console.WriteLine("4 Speed Engaged");
+    }
+
+    public void DisengageClutch()
+    {
+        Console.WriteLine("Disengaging 4 speed clutch.");
+    }
+}
+
+/// <summary>
+/// IGearBox is to get around the transmission returns. Add this type to the GearBox definition
+/// </summary>
+public interface IGearBox
+{
+
+    void ShiftGear();
+}
+
+public class SixSpeed : FourSpeed
+{
+    public override void Engage()
+    {
+        Console.WriteLine("6 Speed Engaged");
+    }
+
+    public void Enable4WheelDrive()
+    {
+        Console.WriteLine("6 Speed Engaged");
     }
 }
 
 public class Person
+{
+    private int Id { get; set; }
+    private string FirstName { get; set; }
+    private string LastName { get; set; }
+    public Person()
+    { }
+    public Person(int id, string firstName, string lastName)
     {
-        private int Id { get; set; }
-        private string FirstName { get; set; }
-        private string LastName { get; set; }
-        public Person()
-        { }
-        public Person(int id, string firstName, string lastName)
-        {
-            Id = id;
-            FirstName = firstName;
-            LastName = lastName;
-        }
-        public virtual Person GetPerson()
-        {
-            return new Person();
-        }
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
     }
+    public virtual Person GetPerson()
+    {
+        return new Person();
+    }
+}
 
 
-    public class Employee : Person
+public class Employee : Person
+{
+    private string Department { get; set; }
+    public Employee()
+    { }
+    public Employee(int id, string firstName, string lastName,
+    string department) : base(id, firstName, lastName)
     {
-        private string Department { get; set; }
-        public Employee()
-        { }
-        public Employee(int id, string firstName, string lastName,
-        string department) : base(id, firstName, lastName)
-        {
-            Department = department;
-        }
-        public override Employee GetPerson()
-        {
-            return new Employee();
-        }
+        Department = department;
     }
+    public override Employee GetPerson()
+    {
+        return new Employee();
+    }
+}
